0ee731d5a0af3beb463e2ec7a2f927471fa9ac01
Nareg Berberian <naregb94@gmail.com>
2022-07-26 12:06:33 +0200

    finished

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
+import { MatDialog } from '@angular/material/dialog';
+import { AddPlayerDialogComponent } from './add-player-dialog/add-player-dialog.component';
 
 @Component({
   selector: 'app-game',
@@ -9,9 +11,9 @@ import { Game } from 'src/models/game';
 export class GameComponent implements OnInit {
   drawACardAnimation = false;
   currentCard: string = '';
-  game = new Game;
+  game = new Game();
 
-  constructor() {}
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.newGame();
@@ -19,9 +21,8 @@ export class GameComponent implements OnInit {
 
   newGame() {
     this.game = new Game();
-    console.log(this.game)
+    console.log(this.game);
   }
-
 
   drawACard() {
     if (!this.drawACardAnimation) {
@@ -29,13 +30,28 @@ export class GameComponent implements OnInit {
       console.log(this.currentCard);
       this.drawACardAnimation = true;
 
-      console.log('new card'+this.currentCard)
-      console.log('game is'+ this.game)
+      console.log('new card' + this.currentCard);
+      console.log('game is' + this.game);
 
+      this.game.currentPlayer++;
+      this.game.currentPlayer =
+        this.game.currentPlayer % this.game.players.length;
       setTimeout(() => {
         this.drawACardAnimation = false;
         this.game.playedCards.push(this.currentCard);
       }, 1000);
     }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(AddPlayerDialogComponent);
+
+    dialogRef.afterClosed().subscribe((name: string) => {
+      if (name && name.length > 0) {
+        this.game.players.push(name);
+        // console.log('The dialog was closed', name);
+      }
+
+    });
   }
 }
